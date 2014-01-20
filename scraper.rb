e7c558a5657429f71f3a672b8e73f6b83134aaf4
Matthew Landauer <matthew@openaustralia.org>
2014-01-20 05:46:13 -0800

    Automatic update to make ScraperWiki scraper work on Morph

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,4 @@
+require 'scraperwiki'
 require 'mechanize'
 require 'uri'
 
@@ -35,7 +36,7 @@ page = agent.get(url)
     next
   end
 
-  if ScraperWiki.select("* from swdata where `council_reference`='#{record['council_reference']}'").empty? 
+  if ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? 
     ScraperWiki.save_sqlite(['council_reference'], record)
   else
     puts "Skipping already saved record " + record['council_reference']
